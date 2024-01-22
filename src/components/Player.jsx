55fbc4ed9572b6f9b12542ed5d6abb97b7c4c212
Elpah <obengelpachris@gmail.com>
2024-01-22 22:21:54 +0100

    initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import { useRef, useState } from "react";
+
+export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const playerName = useRef();
+
+  const handleSubmit = () => {
+    setEnteredPlayerName(playerName.current.value);
+  };
+
+  return (
+    <section className="max-w-md mx-auto my-8 p-6 bg-gray-100 rounded-md shadow-md">
+      <h2 className="text-2xl font-bold mb-4">
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
+      </h2>
+      <p className="mb-4">
+        <input
+          type="text"
+          ref={playerName}
+          className="border border-gray-300 px-3 py-2 rounded-md w-full"
+        />
+      </p>
+      <button
+        onClick={handleSubmit}
+        className="bg-blue-500 text-white px-4 py-2 rounded-md"
+      >
+        Set Name
+      </button>
+    </section>
+  );
+}
